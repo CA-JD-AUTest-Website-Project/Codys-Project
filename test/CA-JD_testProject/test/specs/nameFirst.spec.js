c7631b738e6665b9cc6de08662f635b42161e905
Wom2 <macmini21@MacMini21s-Mac-mini.local>
2023-08-10 15:52:00 -0700

    added default data values

--- test/CA-JD_testProject/test/specs/nameFirst.spec.js
+++ test/CA-JD_testProject/test/specs/nameFirst.spec.js
@@ -2,12 +2,13 @@
  * test with page objects
  */
 import RegisterPage from '../pageobjects/register.page.js'
+import { testValues } from '../testData/testData.js'
 
 
 describe.skip('nameFirst application addresses the firstname field, this positive test suite ', () => {
     it('will ensure allowance, validity of inclusion of XXX requirement', async () => {
         await RegisterPage.open()
-
+        await RegisterPage { testValues.firstNameV.validDefault, testValues.lastNameV.validDefault, testValues.eMailV.validDefault, testValues.password.validDefault, testValues.lastNameV.validDefault, testValues.confirmV.validDefault, testValues.addressV.validDefault, testValues.aptSteUnitV.validDefault, testValues.countryV.validDefault, testValues.cityV.validDefault, testValues.stateV.validDefault, testValues.zipV.validDefault, testValues.phoneV.validDefault}
         //begin valid test cases here
 
         //await LoginPage.login('tomsmith', 'SuperSecretPassword!')
@@ -17,9 +18,16 @@ describe.skip('nameFirst application addresses the firstname field, this positiv
     })
 })
 
+// immediately below is for Canada or other
+describe.skip('zipCanada application addresses the postal code for Canada field, this positive test suite ', () => {
+    it('will ensure allowance, validity of inclusion of XXX requirement', async () => {
+        await RegisterPage.open()
+        await RegisterPage { testValues.firstNameV.validDefault, testValues.lastNameV.validDefault, testValues.eMailV.validDefault, testValues.password.validDefault, testValues.lastNameV.validDefault, testValues.confirmV.validDefault, testValues.addressV.validDefault, testValues.aptSteUnitV.validDefault, testValues.countryV.validDefault, testValues.cityV.validDefault, testValues.provinceV.validDefault, testValues.zipCanadaV.validDefault, testValues.phoneV.validDefault}
+    })
+})
 
 describe.skip('nameFirst application addresses the firstname field, this negative test suite ', () => {
-    it('will determine the correct restriction of XXX as an entry', async () => {
+    it('will determine the correct restriction of TAGXXX as an entry', async () => {
         await RegisterPage.open()
 
         //begin valid test cases here
