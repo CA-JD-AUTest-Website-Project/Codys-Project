c0bfaf8cb7de0cbfef77597bce297a2867dd9bdf
JDiggs1990 <sma115aaccnt@gmail.com>
2023-08-15 13:41:05 -0700

    trying to add firstName.spec.js and testData.js as they were preventing a merge

---
+++ test/CA-JD_testProject/test/testData/testData.js
@@ -0,0 +1,222 @@
+export class testValues {
+
+    //explicitly states what is being tested in IT statement, as well as data var, therefore can use keyword to search excel for TAG
+    // or can search for data value itslf, a better approach
+    static firstNameV {
+    
+        validDefault: 'Günther',   //Test Case 1, first of all valid values
+        valid2: '',     //Test Case 2
+        valid3: '',     //
+    
+    
+    }
+    
+    static firstNameI {     //invalid thingies... Case Type such as firstNameIP, firstNameBVAL, firstNameEP?
+                            //each invalid data for a different test case
+        invalid1: '',      //first invalid name  with Tag ##
+        invalid2: '',          //DO NOT rename as invalid tag ## ? >8
+        invalid3: '',           // if value changes, update excel sheet ASAP, and vosa versa
+                // do not reference specific TAG, say X2, in comment next to data, as would be visually disturbing with all comments for pages and pages being nothing but Xs therefore unreadable
+                // nor aplhaebet soup        
+    
+    
+    }
+    
+    static lastNameV {
+        validDefault: 'Have\'feld',
+        valid2: '',
+    
+    
+    }
+    
+    static lastNameI {
+        invalid1: '',
+    
+    
+    }
+    
+    static eMailV {
+        validDefault: '<Jacksolittlé!>@gmail.com',
+        valid2: '',
+    
+    
+    }
+    
+    static eMailI {
+        invalid1: '',
+    
+    
+    }
+    
+    static passwordV {
+        validDefault: '*Wisew0mbat!*',
+        valid2: '',
+    
+    
+    }
+    
+    static passwordI {
+        invalid1: '',
+    
+    
+    }
+    
+    static confirmV {
+        validDefault: '*Wisew0mbat!*',
+        valid2: '',
+    
+    
+    }
+    
+    static confirmI {
+        invalid1: '',
+    
+    
+    }
+    
+    static addressV {
+        validDefault: '7791 Piña-Pizza St.',
+        valid2: '',
+    
+    
+    }
+    
+    static addressI {
+        invalid1: '',
+    
+    
+    }
+    
+    static aptSteUnitV {
+        validDefault: '#15',
+        valid2: '',
+    
+    
+    }
+    
+    static aptSteUnitI {
+        invalid1: '',
+    
+    
+    }
+    
+    static countryV {
+        validDefault: 'UNITED STATES',
+        valid2: '',
+    
+    
+    }
+    
+    static countryI {
+        invalid1: '',
+    
+    
+    }
+    
+    static cityV {
+        validDefault: 'San Diego',
+        valid2: '',
+    
+    
+    }
+    
+    static cityI {
+        invalid1: '',
+    
+    
+    }
+    
+    static stateV {
+        validDefault: 'CA',
+        valid2: '',
+    
+    
+    }
+    
+    static stateI {
+        invalid1: '',
+    
+    
+    }
+    
+    static provinceV {
+        validDefault: '',
+        valid2: '',
+    
+    
+    }
+    
+    static provinceI {
+        invalid1: '',
+    
+    
+    }
+    
+    static zipV {
+        validDefault: '15214',
+        valid2: '',
+    
+    
+    }
+    
+    static zipI {
+        invalid1: '',
+    
+    
+    }
+    
+    static zipCanadaV {
+        validDefault: '',
+        valid2: '',
+    
+    
+    }
+    
+    static zipCanadaI {
+        invalid1: '',
+    
+    
+    }
+    
+    static zipOtherV {
+        validDefault: '',
+        valid2: '',
+    
+    
+    }
+    
+    static zipOtherI {
+        invalid1: '',
+    
+    
+    }
+    
+    static phoneV {
+        validDefaultPt1: '858',
+        validDefaultPt2: '588',
+        validDefaultPt3: '2300',
+    
+    
+    }
+    
+    static phoneI {
+        invalid1: '',
+    
+    
+    }
+    }
+    
+    
+    /*
+    for Array[ContainsAllFields], n++ through all fields, <Array.length){
+            //enters all valid fields 0 through n
+            if n = field being changed
+                n++
+                x=n
+    
+    then outside for loop, as changing field has been skipped, Array[x] = InvalidValue
+    
+    
+    */
+ }
+}
