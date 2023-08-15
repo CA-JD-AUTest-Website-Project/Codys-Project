32d8bd7b558b7128dcad50a9be99c416849904d9
JDiggs1990 <sma115aaccnt@gmail.com>
2023-08-15 13:44:27 -0700

    gahhhhh git merge origin

--- test/CA-JD_testProject/test/specs/nameFirst.spec.js
+++ test/CA-JD_testProject/test/specs/nameFirst.spec.js
@@ -1,33 +1,32 @@
 /**
  * test with page objects
  */
+
 import RegisterPage from '../pageobjects/register.page.js'
+import { testValues } from '../testData/testData.js'
 
-
-describe.skip('nameFirst application addresses the firstname field, this positive test suite ', () => {
+xdescribe('nameFirst application addresses the firstname field, this positive test suite ', () => {
     it('will ensure allowance, validity of inclusion of XXX requirement', async () => {
         await RegisterPage.open()
-
+        await RegisterPage { testValues.firstNameV.validDefault, testValues.lastNameV.validDefault, \
+            testValues.eMailV.validDefault, testValues.password.validDefault, testValues.lastNameV.validDefault, \
+            testValues.confirmV.validDefault, testValues.addressV.validDefault, testValues.aptSteUnitV.validDefault, \
+            testValues.countryV.validDefault, testValues.cityV.validDefault, testValues.stateV.validDefault, \
+            testValues.zipV.validDefault, testValues.phoneV.validDefault}
         //begin valid test cases here
 
-        //await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        //await expect(SecurePage.flashAlert).toBeExisting()
-       // await expect(SecurePage.flashAlert).toHaveTextContaining(
-       //     'You logged into a secure area!')
+        
     })
 })
 
 
-describe.skip('nameFirst application addresses the firstname field, this negative test suite ', () => {
+
+xdescribe('nameFirst application addresses the firstname field, this negative test suite ', () => {
     it('will determine the correct restriction of XXX as an entry', async () => {
         await RegisterPage.open()
 
         //begin valid test cases here
 
-        //await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        //await expect(SecurePage.flashAlert).toBeExisting()
-       // await expect(SecurePage.flashAlert).toHaveTextContaining(
-       //     'You logged into a secure area!')
+        
     })
 })
-
