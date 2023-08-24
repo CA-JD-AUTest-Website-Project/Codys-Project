8091da005ce6a71f773bbb7dd78e48886f37f453
Wom2 <macmini21@MacMini21s-Mac-mini.local>
2023-08-24 14:16:28 -0700

    almost finished testData file, still need to add sub entries

--- test/CA-JD_testProject/test/testData/testData.js
+++ test/CA-JD_testProject/test/testData/testData.js
@@ -73,119 +73,126 @@ export class testValues {
     
     static addressV = {
         validDefault: '7791 Piña-Pizza St.',
-        valid2: '7791'
-    
-    
+        valid2: '7791',
+        valid4: '8854 oldmcdonaldhadafarmeieioandonthat-farmhehadadogeieiobar St.'
     }
     
     static addressI = {
-        invalid1: ''
-    
-    
+        invalid1: '779',
+        invalid2: '8854 oldmcdonaldhadafarmeieioandonthat-farmhehadadogeieiobark St.',
+        invalid3: '7791 @Piña-Pizza St.'
     }
     
     static aptSteUnitV = {
         validDefault: '#15',
-        valid2: ''
-    
-    
+        valid2: '2345',
+        valid3: '1234567890',
+        valid4: '123'
     }
     
     static aptSteUnitI = {
-        invalid1: ''
-    
-    
+        invalid2: '26',
+        invalid3: '12345678909',
+        invalid4: '025',
+        invalid5: '3',
+        invalid6: '&15',
+        invalid7: '26'
     }
     
     static countryV = {
         validDefault: 'UNITED STATES',
-        valid2: ''
+        valid2: 'Canada',
+        valid3: 'UK'
     
     
     }
     
     static countryI = {
-        invalid1: ''
-    
-    
+        invalid1: 'Select a country'
     }
     
     static cityV = {
         validDefault: 'San Diego',
-        valid2: ''
-    
-    
+        valid2: 'San José',
+        valid3: 'Winston-Salem',
+        valid4: 'Rock',
+        valid5: 'Llangofairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogogogoch'
     }
     
     static cityI = {
-        invalid1: ''
-    
-    
+        invalid1: 'San Diego!',
+        invalid2: 'San Dieg0',
+        invalid3: 'Bet',
+        invalid4: 'Llangofairpwllgwyngyllgogyerychwyrndrobwllllantysiliogogogogogoch'
     }
     
     static stateV = {
         validDefault: 'CA',
-        valid2: ''
-    
-    
+        valid2: 'VA',
+        valid3: 'L'
     }
     
     static stateI = {
-        invalid1: ''
-    
-    
+        invalid1: '45', //corrosponds with state letters
+        invalid2: '&%',
+        invalid3: 'ARI', //will give idaho instead of arkansas
+        invalid4: 'State',
     }
     
     static provinceV = {
-        validDefault: '',
-        valid2: ''
-    
-    
+        validDefault: 'AB',
+        valid2: 'A'
     }
     
     static provinceI = {
-        invalid1: ''
-    
-    
+        invalid1: '23',
+        invalid2: '*^',
+        invalid3: 'Provience',
+        invalid4: 'ONT' // turns from Ontario to Prince Edwards
     }
     
     static zipV = {
         validDefault: '15214',
-        valid2: ''
-    
-    
+        valid2: '00501', //lowest valid zip value
+        valid3: '99950'
     }
     
     static zipI = {
-        invalid1: ''
-    
-    
+        invalid1: '919416803', //no idea where this value came from
+        invalid2: '919',
+        invalid3: '9!941',
+        invalid4: '15ü14', //seems neither of us are sure why this one exists
+        invalid5: '9A941',
+        invalid6: '9 941',
+        invalid7: '9194'
     }
     
-    static zipCanadaV = {
-        validDefault: '',
-        valid2: ''
-    
-    
+    static zipCanadaV = { //Note I have no idea about any of these values 
+        validDefault: 'K1A 0B1', //if somethings wrong bring it up
     }
     
     static zipCanadaI = {
-        invalid1: ''
-    
-    
+        invalid1: 'K1A0B1',
+        invalid2: 'K1A 0B1C',
+        invalid3: 'K1A 0',
+        invalid4: '!1A 0B1'
     }
     
     static zipOtherV = {
-        validDefault: '',
-        valid2: ''
-    
-    
+        validDefault: '15214', //Isn't this the same as US default?
+        valid2: '15214-6803',
+        valid3: '15214ü41251',
+        valid4: '15214 6803',
+        valid5: '152',
+        valid6: '1521441251'
     }
     
     static zipOtherI = {
-        invalid1: ''
-    
-    
+        invalid1: '15',
+        invalid2: '15214041251',
+        invalid3: '1',
+        invalid4: '152140041251',
+        invalid5: '^(`O\')^'
     }
     
     static phoneV = {
